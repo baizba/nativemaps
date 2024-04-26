f36d352bcd19b0aa7ce6a14171b9e7ab35b9cd4e
Vidovic Branislav <branislav.vidovic@ext.a1.at>
2024-04-26 14:59:11 +0200

    basic example working

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 
 export default function App() {
+    let location = {
+        latitude: 44.606388,
+        longitude: 17.859719,
+        latitudeDelta: 0.009,
+        longitudeDelta: 0.009
+    }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <MapView
+            style={StyleSheet.absoluteFillObject}
+            provider={"google"}
+            mapType={"hybrid"}
+            region={location}>
+
+        </MapView>
     </View>
   );
 }
---
+++ README.md
@@ -0,0 +1,5 @@
+<h1>react-native-maps example</h1>
+Simple react native maps example.<br>
+Start with: yarn start<br>
+Install expo: npx expo install<br>
+Install expo on the Phone and scan QR code
